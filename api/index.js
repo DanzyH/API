0dcd9a546b4b9a94508e45bfc48f6cebc5961de9
DanzyH <nguyenviethoang8902@gmail.com>
2023-03-20 01:10:24 +0700

    init

---
+++ api/index.js
@@ -0,0 +1,33 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import userRoutes from "./routes/users.js"
+import postRoutes from "./routes/posts.js"
+import commentRoutes from "./routes/comments.js"
+import likeRoutes from "./routes/likes.js"
+import authRoutes from "./routes/auth.js"
+
+
+const app = express();
+
+// middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true)
+  next()
+})
+app.use(express.json())
+app.use(cors({
+  origin: "http://localhost:3000",
+}))
+app.use(cookieParser())
+
+// router
+app.use("/api/users", userRoutes)
+app.use("/api/posts", postRoutes)
+app.use("/api/comments", commentRoutes)
+app.use("/api/likes", likeRoutes)
+app.use("/api/auth", authRoutes)
+
+app.listen(8080, () => {
+  console.log("API is working!")
+})
